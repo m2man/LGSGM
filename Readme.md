e4b84a13e95693c016d2977867553efe494a0fc0
m2man <nmduy881994@gmail.com>
2021-06-30 10:09:46 +0100

    add main readme

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# A Deep Local and Global Scene Graph Matching for Image-Text Retrieval
+
+This is the repository of the **A Deep Local and Global Scene Graph Matching for Image-Text Retrieval** [paper](https://arxiv.org/abs/2106.02400) which is accepted in SOMET2021. This research is inspired by the SGM [paper](https://arxiv.org/abs/1910.05134) and can be considered as an major improvement of it. The comparison can be fully described in our paper.
+
+Our code is mostly based on the SGM original code.
+
+## 1. Requirements
+Please install packages in the ```requirements.txt```. The project is implemented with python 3.7.9
+
+## 2. Data prepare
+Our data (Flickr30k) is original given by the SGM [paper](https://arxiv.org/abs/1910.05134). We only performed same basic cleaning process to remove duplicated data and lowering text. The preprocessed data can be found in the Data folder.
+
+The model also need the visual features which are the embedded vector of objects in images. In this research, we used EfficientNet-b5 to extract the features. You can extract by running ```extract_visual_features.py``` script. We also uploaded our prepared features ([here](https://drive.google.com/drive/folders/1IvlmTZ9wUpOVIr9MzPgWZB5aYTaTD0jn?usp=sharing)). You can download it and place in the **Data folder**.
+
+## 3. Training and Evaluating
+You can run the ```main_train.py``` script to perform either training or evaluating the model. Our pretrained model can be found [here](https://drive.google.com/drive/folders/100t_GxbhycwfQO82cz-7Xfkn8_t69_Vz?usp=sharing). Please download it and place in **Report folder**.
+
+## 4. Contact
+For any issue or comment, you can directly email me at manh.nguyen5@mail.dcu.ie
--- main_train.py
+++ main_train.py
@@ -11,28 +11,28 @@ info_dict['numb_sample'] = None # training sample for 1 epoch
 info_dict['numb_epoch'] = 100 # number of epoch
 info_dict['numb_gcn_layers'] = 1 # number of gin layers to be stacked
 info_dict['gcn_hidden_dim'] = [] # hidden layer in each gin layer
-info_dict['gcn_output_dim'] = 1024
-info_dict['gcn_input_dim'] = 2048
+info_dict['gcn_output_dim'] = 1024 # graph embedding final dim
+info_dict['gcn_input_dim'] = 2048 # node and edges dim of a graph
 info_dict['batchnorm'] = True
 info_dict['batch_size'] = 128
 info_dict['dropout'] = 0.5
-info_dict['visual_backbone'] = 'b5' # 'b0' or 'b4'
+info_dict['visual_backbone'] = 'b5' # EfficientNet backbone to extract visual features
 info_dict['visual_ft_dim'] = 2048
 info_dict['optimizer'] = 'Adam' # or Adam
 info_dict['learning_rate'] = 3e-4
 info_dict['activate_fn'] = 'swish' # swish, relu, leakyrelu
 info_dict['grad_clip'] = 2 # Gradient clipping
-# info_dict['use_residual'] = False # always set it to false
+# info_dict['use_residual'] = False # always set it to false (not implemented yet)
 # Embedder for each objects and predicates, embed graph only base on objects
-info_dict['model_name'] = 'GCN_ObjAndPredShare_NoFtExModule_LSTM_i2t' 
-info_dict['checkpoint'] = None #'/home/nmduy/Graphs/GraSim/Report/GCN_ObjAndPredShare_NoFtExModule_LSTM-15022021-014523.pth.tar'
+info_dict['model_name'] = 'GCN_ObjAndPredShare_NoFtExModule_LSTM' 
+info_dict['checkpoint'] = None # Training from a pretrained path
 info_dict['margin_matrix_loss'] = 0.35
 info_dict['rnn_numb_layers'] = 2
 info_dict['rnn_bidirectional'] = True
-info_dict['rnn_structure'] = 'LSTM'
+info_dict['rnn_structure'] = 'LSTM' # LSTM or GRU (LSTM gave better result)
 info_dict['graph_emb_dim'] = info_dict['gcn_output_dim']*2
 info_dict['include_pred_ft'] = True # include visual predicate features or not
-info_dict['freeze'] = False
+info_dict['freeze'] = False # Freeze all layers except the graph convolutional network and graph embedding module
 
 def run_train(info_dict):
     if not os.path.exists(info_dict['save_dir']):
